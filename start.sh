51a569f78df5aa32e7097ae8373e9eb34c202579
Nia Schmald <nia.schmald@signifyd.com>
2018-01-05 16:45:36 -0800

    Use hostname environment variable for livy server
    Summary: Use hostname environment variable for livy server
    
    Test Plan: local
    
    Reviewers: dyno.fu
    
    Reviewed By: dyno.fu
    
    Differential Revision: https://signifyd.phacility.com/D10627

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sed -i s"/localhost/$LIVY_HOST/g" .sparkmagic/config.json 
+start-notebook.sh --NotebookApp.iopub_data_rate_limit=1000000000 \
+	--NotebookApp.open_browser=False \
+	--NotebookApp.contents_manager_class='ipymd.IPymdContentsManager' \ 
+	--NotebookApp.log_level='DEBUG' \
