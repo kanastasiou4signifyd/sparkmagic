70585a313cab56bb0ba4763234c686650f210a87
Nia Schmald <nia.schmald@signifyd.com>
2018-02-15 20:53:09 -0800

    Use modeletl library from nexus
    Summary: Use modeletl library from nexus
    
    Test Plan: Local testing
    
    Reviewers: liberty, dyno.fu
    
    Reviewed By: liberty, dyno.fu
    
    Differential Revision: https://signifyd.phacility.com/D11172

--- examples/session_init.scala
+++ examples/session_init.scala
@@ -1,14 +1,12 @@
+import com.signifyd.modeletl.spark.hive.InvestigationMvTables
 import com.signifyd.modeletl.spark.ModelEtlSparkContext
 
-implicit val    modelEtlSparkContext = new ModelEtlSparkContext(spark, Array("/nfs/spark/cfg.yml",
-                                                                             "/nfs/spark/cfg-cassandra-prod.yml",
-                                                                             "/nfs/spark/cfg.CurrentTables.yml"))
-implicit val _spark = modelEtlSparkContext.spark
+val configFiles = spark.conf.getOption("spark.signifyd.CONFIG").getOrElse("/nfs/spark/cfg.yml,/nfs/spark/cfg-cassandra-prod.yml,/nfs/spark/cfg.CurrentTables.yml").split(",")
+println(configFiles)
+implicit val modelEtlSparkContext = new ModelEtlSparkContext(configFiles)
+implicit val spark = modelEtlSparkContext.spark
 
-val hoodieInc = new com.signifyd.modeletl.spark.hive.InvestigationMvHoodieIncremental()
-hoodieInc.tables.investigationMvHoodie.getDataset.createOrReplaceTempView("investigation_mv")
-hoodieInc.tables.rawHoodie.getDataset.createOrReplaceTempView("investigation_raw")
-hoodieInc.tables.orderKeysHoodie.getDataset.createOrReplaceTempView("order_history")
-
-val df = spark.read.orc("s3://com.signifyd.model.hive/tmp/new_raw/raw/data_source=signifyd/data_date=20171101/_VERSION=1510164098224")
-df.createOrReplaceTempView("junk")
+val tables = new InvestigationMvTables()
+tables.investigationMvHoodie.getDataset.createOrReplaceTempView("investigation_mv")
+tables.rawHoodie.getDataset.createOrReplaceTempView("investigation_raw")
+tables.orderKeysHoodie.getDataset.createOrReplaceTempView("order_history")
--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@
 sed -i s"/localhost/$LIVY_HOST/g" .sparkmagic/config.json 
 start-notebook.sh --NotebookApp.iopub_data_rate_limit=1000000000 \
 	--NotebookApp.open_browser=False \
-	--NotebookApp.contents_manager_class='ipymd.IPymdContentsManager' \ 
+	--NotebookApp.contents_manager_class='ipymd.IPymdContentsManager' \
 	--NotebookApp.log_level='DEBUG' \
+	--NotebookApp.notebook_dir=/home/jovyan/work
